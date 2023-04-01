ae4f1a08643fc35f74d39250d6f1cb7aa94065be
Carla Fernandes <carla.sfernandes@outlook.com>
2023-04-01 16:35:47 -0300

    CommitInicial

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,110 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Carla Sousa Fernandes on 30/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   
+    @State var playerCard = "card7"
+    @State var cpuCard = "card13"
+    
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+    
+    var body: some View {
+        
+        ZStack{
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                    
+                }
+                Spacer()
+                
+                
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+
+                
+                Spacer()
+                
+                HStack{
+                    
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                        
+                    }
+                    
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
+            
+            
+        }
+    }
+    
+    func deal() {
+        // Randomize the Players card
+        let playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        
+        // Randomize the CPUs card
+        let cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+        
+        // Update the Scores
+        if playerCardValue > cpuCardValue {
+            
+            // Add 1 to Player Score
+            playerScore += 1
+        }
+        else if cpuCardValue > playerCardValue {
+            // Add 1 to CPU Score
+            cpuScore += 1
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Carla Sousa Fernandes on 30/03/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
